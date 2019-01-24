19f5b4be25f07581a96339e7ee7872f93ea0b89a
Bert Curtis <bert.curtis22@gmail.com>
2019-01-23 18:30:56 -0700

    writing relevant data to arrays

--- phantom-scripts/write-car-data.js
+++ phantom-scripts/write-car-data.js
@@ -18,95 +18,111 @@ var car = {
 	imgs: [],
 	kslurl: ""
 };
+var descriptions = [],
+	carInfoArrs = [],
+	prices = [],
+	imgArrs = [],
+	kslUrls = [],
+	cars = [];
+
 var checkAttribute = function (input) {
 	var parsedInput = input.split(':');
 	switch (parsedInput[0].toLowerCase()) {
-		case 'price':
-			car.price = parsedInput[1];
-			break;
 		case 'vin':
-			car.vin = parsedInput[1];
+			car.vin = parsedInput[1].trim();
 			break;
 		case 'make':
-			car.make = parsedInput[1];
+			car.make = parsedInput[1].trim();
 			break;
 		case 'model':
-			car.model = parsedInput[1];
+			car.model = parsedInput[1].trim();
 			break;
 		case 'year':
-			car.year = parsedInput[1];
+			car.year = parsedInput[1].trim();
 			break;
 		case 'mileage':
-			car.miles = parsedInput[1];
+			car.miles = parsedInput[1].trim();
 			break;
 		case 'trim':
-			car.trim = parsedInput[1];
+			car.trim = parsedInput[1].trim();
 			break;
 		case 'exterior color':
-			car.extcolor = parsedInput[1];
+			car.extcolor = parsedInput[1].trim();
 			break;
 		case 'interior color':
-			car.intcolor = parsedInput[1];
+			car.intcolor = parsedInput[1].trim();
 			break;
 		case 'transmission':
-			car.transmission = parsedInput[1];
+			car.transmission = parsedInput[1].trim();
 			break;
 		case 'liters':
-			car.liters = parsedInput[1];
+			car.liters = parsedInput[1].trim();
 			break;
 		case 'cylinders':
-			car.cylinders = parsedInput[1];
+			car.cylinders = parsedInput[1].trim();
 			break;
 		case 'drive type':
-			car.drivetype = parsedInput[1];
+			car.drivetype = parsedInput[1].trim();
 			break;
 		case 'number of doors':
-			car.numdoors = parsedInput[1];
+			car.numdoors = parsedInput[1].trim();
 			break;
 		case 'fuel type':
-			car.fuel = parsedInput[1];
+			car.fuel = parsedInput[1].trim();
 			break;
 	}
 }
-var cars = [];
 
-var parseCarData = function (data, callback) {
+var parseCarData = function (data) {
+	for (var i = 0; i < data.length; ++i) {
+		if (data[i] === 'description') {
+			descriptions.push(data[i + 1]);
+		}
+		else if (data[i] === 'car-info') {
+			carInfoArrs.push(data[i + 1]);
+		}
+		else if (data[i] === 'img-urls') {
+			imgArrs.push(data[i + 1]);
+		}
+		else if (data[i] === 'url') {
+			kslUrls.push(data[i + 1]);
+		}
+		else if (data[i] === 'price') {
+			prices.push(data[i + 1]);
+		}
+	}
+	/*
 	if (data.startsWith('description')) {
 		car.description = data.split('=')[1];
-		callback(car);
+		return callback(car);
 	}
 	else if (data.startsWith('car-info')) {
-		var carInfos = data.split('=');
+		var infoData = data.split('=');
+		var carInfos = infoData[1].split(',');
 		var infosCount = 0;
 		carInfos.forEach(function (infos) {
 			if (infosCount > carInfos.length) {
-				callback(car);
+				return callback(car);
 			}
 			else {
 				++infosCount;
-				checkAttribute(infos.trim());
+				checkAttribute(infos);
 			}
 		});
 	}
 	else if (data.startsWith('img-urls')) {
 		var imgs = data.split('=');
-		var imgCount = 0;
-		imgs.forEach(function(img) {
-			if (imgCount > carInfos.length) {
-				callback(car);
-			}
-			else {
-				++imgCount;
-				if (img.trim() !== 'img-urls') {
-					car.imgs.push(img);
-				}
-			}
-		});
+		car.imgs = imgs[1].split(',');
+		return callback(car);
 	}
 	else if (data.startsWith('url')) {
 		car.kslurl = data.split('=')[1];
-		callback(car);
+		return callback(car);
 	}
+	else if (data.startsWith('price')) {
+		car.price = data.split('=')[1];
+		return callback(car);
+	}*/
 };
 
 var spawn = require("child_process").spawn;
@@ -152,10 +168,10 @@ populateArgs(function (args) {
 	// Receive output of the child process
 	child.stdout.on("data", function (data) {
 		var textData = Uint8ArrToString(data);
-		//parseCarData(textData, function(car) {
-		//	console.log(car);
-		//});
-		console.log(textData);
+		if (textData.startsWith('poop')) {
+			parseCarData(textData.split('~=~'));
+		}				
+		console.log("textData:   " + textData);
 	});
 
 	// Receive error output of the child process
@@ -166,6 +182,12 @@ populateArgs(function (args) {
 
 	// Triggered when the process closes
 	child.on("close", function (code) {
+		
+		console.log("Length of descriptions arr:" + descriptions.length);
+		console.log("Length of carInfoArrs arr:" + carInfoArrs.length);
+		console.log("Length of prices arr:" + prices.length);
+		console.log("Length of kslUrls arr:" + kslUrls.length);
+		console.log("Length of imgArrs arr:" + imgArrs.length);
 		console.log("Process closed with status code: " + code);
 	});
 });
